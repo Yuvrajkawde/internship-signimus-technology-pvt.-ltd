9547efb5d3289234221802cf4262303fca11a2c1
Yuvrajkawde <111110456+Yuvrajkawde@users.noreply.github.com>
2025-10-01 10:32:09 +0530

    Add files via upload

---
+++ Teacher.java
@@ -0,0 +1,23 @@
+package com.signimus.Student.Managment.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Teacher {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private  Long id;
+    private String teacherName;
+    private String subject;
+    private String email;
+}
---
+++ TeacherServiceImpl.java
@@ -0,0 +1,48 @@
+package com.signimus.Student.Managment.service.studentService;
+
+import com.signimus.Student.Managment.entity.Teacher;
+import com.signimus.Student.Managment.repositories.TeacherRepository;
+import lombok.Builder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@Builder
+public class TeacherServiceImpl implements TeacherServiceInterface{
+
+    private TeacherRepository teacherRepository;
+
+    @Autowired
+    public void setTeacherReposirory(TeacherRepository teacherReposirory) {
+        this.teacherRepository = teacherReposirory;
+    }
+
+    @Override
+    public Teacher saveTeacher(Teacher teacher) {
+
+        Teacher teacher1 = teacherRepository.save(teacher);
+        return teacher1;
+    }
+
+
+
+    @Override
+    public Teacher updateTeacher(Teacher teacher, Long id) {
+        Optional<Teacher> optionalTeacher = teacherRepository.findById(id);
+        if(optionalTeacher.isPresent()){
+            Teacher teacher1 = optionalTeacher.get();
+           teacher1.setTeacherName(teacher.getTeacherName());
+            teacher1.setEmail(teacher.getEmail());
+            teacher1.setSubject(teacher.getSubject());
+
+
+
+            return teacherRepository.save(teacher1);
+        }else {
+            throw new RuntimeException("user not found");
+        }
+
+}
+}
