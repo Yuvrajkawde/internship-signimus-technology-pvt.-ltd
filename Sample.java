0213f82296a560560968e70deb683c82d78064cb
Yuvrajkawde <111110456+Yuvrajkawde@users.noreply.github.com>
2025-10-09 11:32:36 +0530

    Add files via upload

---
+++ Sample.java
@@ -0,0 +1,14 @@
+package com.demoproject.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Sample {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello world";
+    }
+
+}
